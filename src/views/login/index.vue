ff7f87a76849e436abe155abde3a54958b6918be
未松松 <1721705261@qq.com>
2021-12-29 14:25:49 +0800

    修改登录中文报错

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -60,14 +60,14 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入正确的用户名'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码最少6位！'))
       } else {
         callback()
       }
@@ -116,7 +116,7 @@ export default {
             this.loading = false
           })
         } else {
-          console.log('error submit!!')
+          console.log('用户名或密码错误!!')
           return false
         }
       })
