548446ad968a5ece8466a41751b27d62be2aea0c
未松松 <1721705261@qq.com>
2022-01-25 09:59:05 +0800

    修改公司管理

--- src/views/system/group/index.vue
+++ src/views/system/group/index.vue
@@ -1,31 +1,28 @@
 <template>
   <div class="wrap">
-    <el-card class="box-card">
-      <el-row style="text-align: right;padding-bottom: 18px;">
-        <template v-if="device !== 'mobile'">
-          <el-input
-            placeholder="请输入公司名称"
-            prefix-icon="el-icon-search"
-            style="display: inline-block;width: 250px;margin-right: 12px;"
-          ></el-input>
-        </template>
-        <el-button type="primary">新增公司</el-button>
-        <el-button type="danger">删除</el-button>
-      </el-row>
+    <el-row style="text-align: right;padding: 10px;background: #fff;">
+      <template v-if="device !== 'mobile'">
+        <el-input placeholder="请输入公司名称" prefix-icon="el-icon-search" style="display: inline-block;width: 250px;margin-right: 12px;" />
+      </template>
+      <el-button type="primary">新增公司</el-button>
+      <el-button type="danger">删除</el-button>
+    </el-row>
 
-      <el-row>
-        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-          <div class="card-panel">
-            <div class="panel front">
-              2
-            </div>
-            <div class="panel back">
-              1
-            </div>
+    <el-row>
+      <el-col :xs="24" :sm="24" :lg="6" class="card-panel-col">
+        <div class="card-panel">
+          <div class="panel front">
+            <div class="title">雄宇集团</div>
+            <div class="title">1</div>
           </div>
-        </el-col>
-      </el-row>
-    </el-card>
+          <div class="panel back">
+            <button class="btn">管理</button>
+            <button class="btn">编辑</button>
+            <button class="btn delete">删除</button>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -70,11 +67,14 @@ export default {
 <style lang="scss" scoped>
 .wrap {
   padding: 10px;
-  background-color: rgb(240, 242, 245);
+  background-color: #ecf0f9;
   position: relative;
+
   .card-panel {
     position: relative;
     height: 400px;
+    perspective: 1000px;
+
     .panel {
       position: absolute;
       left: 10px;
@@ -86,19 +86,63 @@ export default {
       border-radius: 10px;
       overflow: hidden;
     }
+
     .front {
+      background: url('~@/assets/images/group/group_front.png') no-repeat;
       background-size: 100% 100%;
     }
+
     .back {
-      background: url('~@/assets/images/group/group_back.png') no-repeat;
+      background: #fff;
       background-size: 100% 100%;
       transform: rotateY(-180deg);
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      padding: 60px 0;
+      .btn {
+        transform: translateZ(40px);
+        cursor: pointer;
+        font-weight: bold;
+        color: #fff;
+        padding: 0.75em 1em;
+        border-radius: 100px;
+        font: inherit;
+        border: none;
+        position: relative;
+        transform-style: preserve-3d;
+        transition: 300ms ease;
+        background: linear-gradient(135deg, #1a9be6, #1a57e6);
+        width: 60%;
+        &::before {
+          transition: 300ms ease;
+          position: absolute;
+          display: block;
+          content: '';
+          transform: translateZ(-40px);
+          height: calc(100% - 20px);
+          width: calc(100% - 20px);
+          border-radius: 100px;
+          left: 10px;
+          top: 16px;
+          box-shadow: 0 0 10px 10px rgb(26 87 230 / 25%);
+        }
+      }
+      .delete {
+        background: linear-gradient(135deg, #e61a80, #e61a3c);
+        &::before {
+          box-shadow: 0 0 10px 10px rgb(230 26 60 / 25%);
+        }
+      }
     }
   }
-  .card-panel:hover .front{
+
+  .card-panel:hover .front {
     transform: rotateY(180deg);
   }
-  .card-panel:hover .back{
+
+  .card-panel:hover .back {
     transform: rotateY(0deg);
   }
 }
