68cd155bba38dde988a3c8123eb148f5f7bed642
未松松 <1721705261@qq.com>
2022-02-11 00:28:10 +0800

    修改

--- src/views/exam/components/userTab.vue
+++ src/views/exam/components/userTab.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <el-tabs type="border-card" tab-position="top" :value="type" @tab-click='toggleType'>
+    <el-tabs type="border-card" tab-position="top" :value="type" @tab-click="toggleType">
       <el-tab-pane label="按学员">
         <el-row style="text-align: right;padding-bottom: 18px;">
-          <el-date-picker v-model="exam_time" type="date" placeholder="筛选考试日期" style="margin-right: 10px;" @change='fetchData'>
-          </el-date-picker>
+          <el-date-picker
+            v-model="exam_time"
+            type="date"
+            placeholder="筛选考试日期"
+            style="margin-right: 10px;"
+            @change="fetchData"
+          />
           <el-button size="medium" type="success">开班</el-button>
           <el-button size="medium" type="primary">设置</el-button>
           <el-button size="medium" type="warning">导出</el-button>
           <el-button size="medium" type="danger">删除</el-button>
         </el-row>
         <el-table v-loading="listLoading" :data="list" element-loading-text="Loading" border fit highlight-current-row>
-          <el-table-column type="selection" width="65" align="center"></el-table-column>
+          <el-table-column type="selection" width="65" align="center" />
           <el-table-column label="学员姓名" align="center">
             <template slot-scope="scope">
               <span>{{ scope.row.contacts }}</span>
@@ -35,32 +40,51 @@
           </el-table-column>
           <el-table-column class-name="status-col" label="状态" align="center" width="150">
             <template slot-scope="scope">
-              <el-tag :type="scope.row.status | statusTypeFilter">{{scope.row.status | statusFilter}}</el-tag>
+              <el-tag v-if="!scope.row.isEdit" :type="scope.row.status | statusTypeFilter">{{ scope.row.status | statusFilter }}</el-tag>
+              <el-select v-if="scope.row.isEdit" v-model="scope.row.status" placeholder="请选择" @change="editType(scope.row)">
+                <el-option key="0" label="排队中" value="0" />
+                <el-option key="1" label="已报名" value="1" />
+                <el-option key="2" label="待报道" value="2" />
+                <el-option key="3" label="已报道" value="3" />
+                <el-option key="4" label="已通过" value="4" />
+                <el-option key="5" label="未通过" value="5" />
+              </el-select>
             </template>
           </el-table-column>
           <el-table-column align="center" label="操作" width="200">
             <template slot-scope="scope">
-              <el-link type="primary" :underline="false" icon="el-icon-edit" style="margin: 0 10px;">编辑</el-link>
+              <el-link type="primary" :underline="false" icon="el-icon-edit" style="margin: 0 10px;" :disabled="scope.row.isEdit" @click="scope.row.isEdit=true">编辑状态</el-link>
               <el-link type="danger" :underline="false" icon="el-icon-delete">删除</el-link>
             </template>
           </el-table-column>
         </el-table>
         <div style="text-align: center;margin-top: 10px;">
-          <el-pagination background hide-on-single-page :page-size="pagination.pageSize"
-            :current-page="pagination.pageNum" layout="prev, pager, next" :total="pagination.total"></el-pagination>
+          <el-pagination
+            background
+            hide-on-single-page
+            :page-size="pagination.pageSize"
+            :current-page="pagination.pageNum"
+            layout="prev, pager, next"
+            :total="pagination.total"
+          />
         </div>
       </el-tab-pane>
       <el-tab-pane label="按创建人">
         <el-row style="text-align: right;padding-bottom: 18px;">
-          <el-date-picker v-model="exam_time" type="date" placeholder="筛选考试日期" style="margin-right: 10px;" @change='fetchData'>
-          </el-date-picker>
+          <el-date-picker
+            v-model="exam_time"
+            type="date"
+            placeholder="筛选考试日期"
+            style="margin-right: 10px;"
+            @change="fetchData"
+          />
           <el-button size="medium" type="success">开班</el-button>
           <el-button size="medium" type="primary">设置</el-button>
           <el-button size="medium" type="warning">导出</el-button>
           <el-button size="medium" type="danger">删除</el-button>
         </el-row>
         <el-table v-loading="listLoading" :data="list" element-loading-text="Loading" border fit highlight-current-row>
-          <el-table-column type="selection" width="65" align="center"></el-table-column>
+          <el-table-column type="selection" width="65" align="center" />
           <el-table-column label="创建人昵称" align="center">
             <template slot-scope="scope">
               <span>{{ scope.row.name }}</span>
@@ -78,7 +102,7 @@
           </el-table-column>
           <el-table-column label="人数" align="center">
             <template slot-scope="scope">
-              {{scope.row.examination?scope.row.examination.length:'--'}}
+              {{ scope.row.examination?scope.row.examination.length:'--' }}
             </template>
           </el-table-column>
           <el-table-column align="center" label="操作" width="200">
@@ -88,113 +112,125 @@
           </el-table-column>
         </el-table>
         <div style="text-align: center;margin-top: 10px;">
-          <el-pagination background hide-on-single-page :page-size="pagination.pageSize"
-            :current-page="pagination.pageNum" layout="prev, pager, next" :total="pagination.total"></el-pagination>
+          <el-pagination
+            background
+            hide-on-single-page
+            :page-size="pagination.pageSize"
+            :current-page="pagination.pageNum"
+            layout="prev, pager, next"
+            :total="pagination.total"
+          />
         </div>
       </el-tab-pane>
     </el-tabs>
+
   </div>
 </template>
 
 <script>
-  import {
-    getList,
-    getCreateList
-  } from '@/api/exam'
-  import {
-    mapGetters
-  } from 'vuex'
+import {
+  getList,
+  getCreateList, edit
+} from '@/api/exam'
+import {
+  mapGetters
+} from 'vuex'
 
-  const defaultNotice = {
-    title: '',
-    image: undefined,
-    note: ''
-  }
-  export default {
-    computed: {
-      ...mapGetters(['device'])
-    },
-    props: ['status'],
-    filters: {
-      statusFilter(status) {
-        const statusMap = {
-          0: '排队中',
-          1: '已报名',
-          2: '待报道',
-          3: '已报道',
-          4: '已通过',
-          5: '未通过'
-        }
-        return statusMap[status]
-      },
-      statusTypeFilter(status) {
-        const statusMap = {
-          0: 'info',
-          1: 'gray',
-          2: 'warning',
-          3: 'info',
-          4: 'success',
-          5: 'danger'
-        }
-        return statusMap[status]
+const defaultNotice = {
+  title: '',
+  image: undefined,
+  note: ''
+}
+export default {
+  computed: {
+    ...mapGetters(['device'])
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        0: '排队中',
+        1: '已报名',
+        2: '待报道',
+        3: '已报道',
+        4: '已通过',
+        5: '未通过'
       }
+      return statusMap[status]
     },
-    data() {
-      return {
-        list: null,
-        type: '0',
-        listLoading: true,
-        exam_time: '',
-        noticeModel: Object.assign({}, defaultNotice),
-        dialogVisible: false,
-        dialogType: 'add',
-        pagination: {
-          total: 0,
-          pageSize: 10,
-          pageNum: 0
-        }
+    statusTypeFilter(status) {
+      const statusMap = {
+        0: 'info',
+        1: 'gray',
+        2: 'warning',
+        3: 'info',
+        4: 'success',
+        5: 'danger'
       }
-    },
-    created() {
-      this.fetchData()
-    },
-    methods: {
-      toggleType(t) {
-        this.type = t.index
-        this.fetchData()
-      },
-      fetchData() {
-        this.listLoading = true
-        if (this.type == '0') {
-          getList({
-            status: this.status,
-            exam_time:this.exam_time
-          }).then(response => {
-            this.list = response.data.data
-            this.listLoading = false
-          })
-        } else {
-          getCreateList({
-            status: this.status,
-            exam_time:this.exam_time
-          }).then(response => {
-            this.list = response.data.data
-            this.listLoading = false
-          })
-        }
-
-      },
-      handleAddClick() {
-        this.noticeModel = Object.assign({}, defaultNotice)
-        this.dialogType = 'new'
-        this.dialogVisible = true
-      },
-      changeUpload(file) {
-        this.imageUrl = URL.createObjectURL(file.raw)
-        this.noticeModel.image = file.raw
+      return statusMap[status]
+    }
+  },
+  props: ['status'],
+  data() {
+    return {
+      list: null,
+      type: '0',
+      listLoading: true,
+      exam_time: '',
+      noticeModel: Object.assign({}, defaultNotice),
+      dialogVisible: false,
+      dialogType: 'add',
+      pagination: {
+        total: 0,
+        pageSize: 10,
+        pageNum: 0
       }
     }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    toggleType(t) {
+      this.type = t.index
+      this.fetchData()
+    },
+    fetchData() {
+      this.listLoading = true
+      if (this.type == '0') {
+        getList({
+          status: this.status,
+          exam_time: this.exam_time
+        }).then(response => {
+          this.list = response.data.data
+          if (this.list && this.list.length > 0) {
+            this.list.forEach(item => {
+              item.isEdit = false
+            })
+          }
+          this.listLoading = false
+        })
+      } else {
+        getCreateList({
+          status: this.status,
+          exam_time: this.exam_time
+        }).then(response => {
+          this.list = response.data.data
+          this.listLoading = false
+        })
+      }
+    },
+    editType(item) {
+      edit({ exam_id: item.exam_id, status: item.status }).then(res => {
+        this.list.forEach(i => {
+          if (i.exam_id == item.exam_id) {
+            i.isEdit = false
+          }
+        })
+      })
+      this.fetchData()
+    }
   }
+}
 </script>
 <style lang="scss" scoped>
   .wrap {
